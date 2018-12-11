511c9d6650edcf0b97dcb4d2a53e65bb29ca7381
Alexis Juven <alexis.juven@enseirb-matmeca.fr>
2018-12-11 09:52:00 +0100

    otsu

--- find_a_ball.cpp
+++ find_a_ball.cpp
@@ -20,11 +20,34 @@ Mat loadPicture(const char * const path)
 }
 
 
+
+Mat loadGrayPicture(const char * const path)
+{
+	Mat output = imread(path, CV_LOAD_IMAGE_GRAYSCALE);
+
+	if (output.data == NULL){
+		std::cerr << "Invalid picture path : " << path << '\n';
+		exit(EXIT_FAILURE);
+	}
+
+	return output;
+}
+
+
+
 void process(const char * const imPath)
 {
 	Mat m = loadPicture(imPath);
 
 	imshow(imPath, m);
+
+
+	Mat greyM = loadGrayPicture(imPath);
+	imshow(imPath, greyM);
+
+	Mat thres_m;
+	/*double found_thred =*/ threshold(greyM, thres_m, 0, 255, THRESH_BINARY + THRESH_OTSU);
+	imshow("otsu-th-ocv.png", thres_m);
 
 	waitKey(0);
 }
