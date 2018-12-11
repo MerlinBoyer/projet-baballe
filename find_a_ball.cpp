cd13e7a96545b7a7c7c456ed1de93d44d9a2cc4c
JUVEN Alexis <ajuven@fischer.pedago.ipb.fr>
2018-12-11 09:03:30 +0100

    find a ball

---
+++ find_a_ball.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+
+Mat loadPicture(const char * const path)
+{
+	Mat output = imread(path, CV_LOAD_IMAGE_COLOR);
+
+	if (output.data == NULL){
+		std::cerr << "Invalid picture path : " << path << '\n';
+		exit(EXIT_FAILURE);
+	}
+
+	return output;
+}
+
+
+void process(const char * const imPath)
+{
+	Mat m = loadPicture(imPath);
+
+}
+
+
+
+
+
+
+
+
+int main(int argc, char * argv[])
+{
+
+	if (argc != 2){
+		cerr << "Usage : " << argv[0] << "imPath\n";
+		exit(EXIT_FAILURE);
+	}
+
+	const char * const imPath = argv[1];
+
+	process(imPath);
+
+	return 0;
+}
+
